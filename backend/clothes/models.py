b860b2fdddc27773c71fc98784e2f4d730c74944
Dinislam <aikenamuteshova@gmail.com>
2023-08-17 18:29:32 +0600

    Add first api

--- backend/clothes/models.py
+++ backend/clothes/models.py
@@ -1,8 +1,12 @@
+from django.contrib.auth.models import User
 from django.db import models
 
 
 class Category(models.Model):
     name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
 
 
 class Product(models.Model):
@@ -12,9 +16,20 @@ class Product(models.Model):
     price = models.DecimalField(max_digits=10, decimal_places=2)
     image = models.ImageField(upload_to='product_images/')
 
+    def __str__(self):
+        return self.name
+
 
 class Review(models.Model):
     product = models.ForeignKey(Product, on_delete=models.CASCADE)
     author = models.CharField(max_length=100)
     text = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
+
+
+class SubCategory(models.Model):
+    name = models.CharField(max_length=100)
+    cat = models.ForeignKey('Category', on_delete=models.PROTECT, null=True, verbose_name="Категории")
+
+    def __str__(self):
+        return self.name

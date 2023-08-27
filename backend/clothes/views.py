37ff0f96b2a5bf7c504ca1ef5d5853c9d793b27c
Dinislam <aikenamuteshova@gmail.com>
2023-08-27 16:18:40 +0600

    blablabla

--- backend/clothes/views.py
+++ backend/clothes/views.py
@@ -1,12 +1,15 @@
+from django.forms import model_to_dict
 from django.shortcuts import render
-from rest_framework import generics
-
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from PIL import Image
 from .models import Product
 from .serializers import ProductSerializer
 
 
 # Create your views here.
 
-class ProductAPIView(generics.ListAPIView):
+class ProductAPIView(APIView):
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
+
